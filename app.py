bd4fe52741ebce690284564cfb040a17f601832f
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-26 06:13:12 +0300

    libbitsandbytes_cuda117

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 os.system(f"git lfs install")
-os.system(f"cp /home/demo/venv/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cuda118.so /home/demo/venv/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cpu.so")
+os.system(f"cp /home/demo/venv/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cuda117.so /home/demo/venv/lib/python3.8/site-packages/bitsandbytes/libbitsandbytes_cpu.so")
 
 os.chdir(f"/home/demo/source")
 os.system(f"pip install -r requirements.txt")
